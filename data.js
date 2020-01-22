7e6f87807d223ccc1179fe0cd672cb6659d162bd
Amelia Hill <amehi4yus@gmail.com>
2020-01-22 12:50:37 -0500

    Added data that will populate table.

---
+++ data.js
@@ -0,0 +1,85 @@
+var CLASS_SCHEDULE = [
+       {
+              Week: 1,
+              Begins: "03/04",
+              Topics: ["Introduction", "Chapter 1: Values Types Operators", "Assignment 1 due 11:59pm 03/10"]
+        },
+        
+        {
+              Week: 2,
+              Begins: "03/11",
+              Topics: ["Chapter 2: Programming Structure", "Assignment 2 due 11:59pm 03/17"]
+        },
+        
+ 	    {
+              Week: 3,
+              Begins: "03/18",
+              Topics: ["Chapter 3: Functions", "Assignment 3 due 11:59pm 03/24/10"]
+        },
+        
+ 	    {
+              Week: 4,
+              Begins: "03/25",
+              Topics: ["Chapter 4: Data Structures//Objects and Arrays", "Assignment 4 due 11:59pm 03/31"]
+        },
+        
+ 	    {
+              Week: 5,
+              Begins: "04/01",
+              Topics: ["Chapter 5: Higher-Order Functions", "Assignment 5 due 11:59pm 04/07", " Test 1: Wednesday 12am (04/03) - Friday 11:59pm (04/05)"]
+        },
+        
+ 	    {
+              Week: 6,
+              Begins: "04/08",
+              Topics: ["Spring Break! No Reading & No Assignment"]
+        },
+        
+ 	    {
+              Begins: "04/15",
+              Topics: ["Chapter 6: The Secret Life of Objects", "Chapter 8: Bugs and Error Handling", "Chapter 10: Modules", "Assignment 6 due 11:59pm 04/14"]
+        },
+ 
+ 	   {
+              Week: 7,
+              Begins: "04/22",
+              Topics: ["Chapter 12: JavaScript and the Browser", "Chapter 13: The Document Object Model", "Assignment 7 due 11:59pm 04/28"]
+        },
+        
+ 	   {
+              Week: 8,
+              Begins: "04/29",
+              Topics: ["Chapter 14: Handling Events", "Assignment 8 due 11:59pm 05/05"]
+        },
+        
+ 	   {
+              Week: 9,
+              Begins: "05/06",
+              Topics: ["Test 2: Monday 12am (05/06) - Tuesday 11:59pm (05/07)", "Chapter 17: HTTP", "Chapter 18: Forms and Form Fields", "Assignment 9 due 11:59pm 05/12"]
+        },
+        
+        {
+              Week: 10,
+              Begins: "05/13",
+              Topics: ["Chapter 20: Node.js//Express.js", "Assignment 10 due 11:59pm 05/19"]
+        },
+        
+        {
+              Week: 11,
+              Begins: "05/20",
+              Topics: ["JQuery//AJAX", "Bonus Assignment due 11:59pm 05/26"]
+        },
+        
+	    {
+              Week: 12,
+              Begins: "05/27",
+              Topics: ["Memorial Day Holiday (05/28)", "Test 3: Tuesday 12am (05/28) - Wednesday 11:59pm (05/29)", "Friday (05/31): Final Project due in Canvas by 11:59 PM", "No Assignment"]
+        },
+        
+	    {
+              Begins: "06/02",
+              Topics: ["COURSE ENDS!"]
+        },
+        
+      ];
+    
